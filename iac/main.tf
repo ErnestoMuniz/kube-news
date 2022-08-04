469a392f548430cdbb3f2850973bf96e64bc88af
ErnestoOnline <ernesto.muniz@online.net.br>
2022-08-04 18:14:35 -0300

    :sparkle: Adicionado deploy com Terraform

---
+++ iac/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.21.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
+  name   = "k8s-iniciativa"
+  region = "nyc1"
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+  }
+}
+
+resource "local_file" "kube_config" {
+    content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
+    filename = "kube_config.yaml"
+}
+
+variable "do_token" {}
